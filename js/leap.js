e2e2f3f6a2428f0d04c21681968565719bd2a923
David Melvin <davidmelvinjr@gmail.com>
2014-10-12 07:28:14 -0400

    centering

--- js/leap.js
+++ js/leap.js
@@ -4,6 +4,10 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+function pseudoSubmit(){
+    //if (event.keycode == 13) document.getElementId('submit').click()
+    console.log(event.keycode);
+}
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
